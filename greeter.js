84ee4370813963ff5040a590cc61c2b841749417
Austin Noronha <austin_noronha@hotmail.com>
2016-11-04 18:04:56 +0530

    Level 5 - Full Example

--- greeter.js
+++ greeter.js
@@ -1,20 +1,21 @@
 /*
-Classes
+Classes with HTML Example
 
 Finally, lets extend the example one last time with classes.
 TypeScript supports new features in JavaScript, like support for class-based object-oriented programming.
 */
 var Student = (function () {
-    function Student(firstName, middleInitial, lastName) {
+    function Student(firstName, middleInitial, lastName, mobileNo) {
         this.firstName = firstName;
         this.middleInitial = middleInitial;
         this.lastName = lastName;
-        this.fullName = firstName + " " + middleInitial + " " + lastName;
+        this.mobileNo = mobileNo;
+        this.fullName = firstName + " " + middleInitial + " " + lastName + " " + mobileNo;
     }
     return Student;
 }());
 function greeter(person) {
-    return "Hello, " + person.firstName + " " + person.lastName;
+    return "Hello, " + person.middleInitial + " " + person.firstName + " " + person.lastName + ", Mobile no: " + person.mobileNo;
 }
-var user = new Student("Jane", "M.", "User");
+var user = new Student("Jane", "M.", "User", "98989899989898");
 document.body.innerHTML = greeter(user);
--- greeter.ts
+++ greeter.ts
@@ -1,5 +1,5 @@
 /*
-Classes
+Classes with HTML Example
 
 Finally, lets extend the example one last time with classes. 
 TypeScript supports new features in JavaScript, like support for class-based object-oriented programming.
@@ -7,20 +7,24 @@ TypeScript supports new features in JavaScript, like support for class-based obj
 
 class Student {
     fullName: string;
-    constructor(public firstName, public middleInitial, public lastName) {
-        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    constructor(public firstName, public middleInitial, public lastName, public mobileNo) {
+        this.fullName = firstName + " " + middleInitial + " " + lastName + " " + mobileNo;
     }
 }
 
 interface Person {
+    middleInitial: string;
     firstName: string;
     lastName: string;
+    fullName: string;
+    mobileNo: string;
 }
 
 function greeter(person : Person) {
-    return "Hello, " + person.firstName + " " + person.lastName;
+    return "Hello, " + person.middleInitial + " " + person.firstName + " " + person.lastName+ ", Mobile no: " + person.mobileNo;
 }
 
-var user = new Student("Jane", "M.", "User");
+
+var user = new Student("Jane", "M.", "User", "98989899989898");
 
 document.body.innerHTML = greeter(user);
