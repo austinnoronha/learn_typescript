af9c38be7620cc6b8302e48f62809ac243689e6c
Austin Noronha <austin_noronha@hotmail.com>
2016-11-04 16:36:32 +0530

    Level 1 - Building your first TypeScript file

---
+++ greeter.js
@@ -0,0 +1,17 @@
+/*
+Building your first TypeScript file
+
+In your editor, type the following JavaScript code in greeter.ts:
+
+Compiling your code
+
+CMD: tsc greeter.ts
+
+The result will be a file greeter.js which contains the same JavaScript that you fed in. We�re up and running using TypeScript in our JavaScript app!
+
+*/
+function greeter(person) {
+    return "Hello, " + person;
+}
+var user = "Jane User";
+document.body.innerHTML = greeter(user);
